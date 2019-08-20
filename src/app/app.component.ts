12cb3a9f17f8cf7229f3d497931720529527b367
Bruce Braithwaite <bruce.braithwaite@healthcatalyst.com>
2019-08-20 16:39:15 -0600

    Sample options added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MultiSelectOption } from './multi-select/multi-select-option';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'multi-select';
+  sampleOptions: MultiSelectOption[] = [
+    { id: '1', label: 'Ford Galaxy' },
+    { id: '2', label: 'VW MicroBus' },
+    { id: '3', label: 'Mercury Tracer' },
+    { id: '5', label: 'Honda Accord' },
+    { id: '4', label: 'Isuzu Trooper' },
+    { id: '6', label: 'Ford Freestar' },
+    { id: '7', label: 'Nissan Frontier' },
+    { id: '8', label: 'Nissan Rogue' },
+    { id: '9', label: 'Scion xB' },
+    { id: '10', label: 'Fiat X19' },
+    { id: '11', label: 'BMW 2002' }
+  ];
 }
