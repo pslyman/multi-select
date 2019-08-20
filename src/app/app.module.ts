4e48288d974e5e9b066c004d565d2911ab7a06a0
Bruce Braithwaite <bruce.braithwaite@healthcatalyst.com>
2019-08-20 14:11:26 -0600

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MultiSelectComponent } from './multi-select/multi-select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultiSelectComponent
   ],
   imports: [
     BrowserModule
